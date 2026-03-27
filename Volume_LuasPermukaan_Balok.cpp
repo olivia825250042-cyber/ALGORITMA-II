f610746093956746e3adc88427aa508f6a254ea2
olivia825250042-cyber <olivia.825250042@stu.untar.ac.id>
2026-03-27 23:34:28 +0700

    Add files via upload

---
+++ Volume_LuasPermukaan_Balok.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float s, volume, luas;
+    printf("Masukkan sisi: ");
+    scanf("%f", &s);
+
+    // Hitung volume dan luas permukaan
+    volume = s * s * s;
+    luas = 6 * s * s;
+    printf("Volume Kubus = %.2f\n", volume);
+    printf("Luas Permukaan Kubus = %.2f\n", luas);
+    return 0;
+}
