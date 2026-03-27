461240af0359bd82aed9a14fbced1494fa92ca67
olivia825250042-cyber <olivia.825250042@stu.untar.ac.id>
2026-03-27 23:38:58 +0700

    Update Volume_LuasPermukaan_Balok.cpp

--- Volume_LuasPermukaan_Balok.cpp
+++ Volume_LuasPermukaan_Balok.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    float s, volume, luas;
-    printf("Masukkan sisi: ");
-    scanf("%f", &s);
+    float p, l, t, volume, luas;
+    printf("Masukkan panjang: ");
+    scanf("%f", &p);
+    printf("Masukkan lebar: ");
+    scanf("%f", &l);
+    printf("Masukkan tinggi: ");
+    scanf("%f", &t);
 
     // Hitung volume dan luas permukaan
-    volume = s * s * s;
-    luas = 6 * s * s;
-    printf("Volume Kubus = %.2f\n", volume);
-    printf("Luas Permukaan Kubus = %.2f\n", luas);
+    volume = p * l * t;
+    luas = 2 * (p*l + p*t + l*t);
+    printf("Volume Balok = %.2f\n", volume);
+    printf("Luas Permukaan Balok = %.2f\n", luas);
     return 0;
 }
