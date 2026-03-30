61c61586a8362e013966305f0a8e9f3513764b20
olivia825250042-cyber <olivia.825250042@stu.untar.ac.id>
2026-03-31 01:01:03 +0700

    Add files via upload

---
+++ Soal1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, a;
+    printf("Masukan jumlah angka: ");
+    scanf("%d", &n);
+
+    for (int i=1; i<=n; i++){
+        printf("%d", n);
+        a +=n;
+    }
+    return 0;
+}
