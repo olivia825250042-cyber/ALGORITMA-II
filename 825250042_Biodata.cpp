3c0e5e7a6308f5d5b3ad6b50a91359bc3511e8c8
olivia825250042-cyber <olivia.825250042@stu.untar.ac.id>
2026-03-31 00:54:35 +0700

    Add files via upload

---
+++ 825250042_Biodata.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    printf("BIODATA DIRI\n"); 
+    printf("Nama: Olivia Anastasha Setiawan\n");
+    printf("Jenis Kelamin: Perempuan\n");
+    printf("Nama Panggilan: Olip\n");
+    printf("Tanggal Lahir: 10 November 2006\n");
+    printf("Umur: 18 tahun\n");
+    printf("Alamat: Jelambar Jaya 2 Gang 15 No 38\n");
+    printf("Hobi: Coba hal baru, Membaca, Menggambar, Melukis\n");
+    printf("Kampus Sekolah: Universitas Tarumanegara\n");
+    printf("Fakultas: FTI\n");
+    printf("Jurusan: Sistem Informasi\n");
+    printf("Cita-cita: Ceo Perusahaan\n");
+    printf("No Telepon: 089623703506\n");
+
+    return 0;
+
+}
